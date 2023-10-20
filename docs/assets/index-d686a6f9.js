00129dae1d0130e11252b609d6c38bfceba3858c
chan-w <40780153+chan-w@users.noreply.github.com>
2023-10-20 00:03:15 -0500

    Fix example comments

--- docs/assets/index-d686a6f9.js
+++ docs/assets/index-d686a6f9.js
@@ -25,7 +25,7 @@ var kf=Object.defineProperty;var Tf=(n,e,t)=>e in n?kf(n,e,{enumerable:!0,config
             {{left = mid + 1}}
     return result
     `,extensions:[cs,Jn,ps(),v.editable.of(!1),v.lineWrapping],parent:document.getElementById("bsearch-any")});new v({doc:`def search_first(arr, target):
-    """search_any([1, 2, 2, 2, 3], 2) => 1"""
+    """search_first([1, 2, 2, 2, 3], 2) => 1"""
     left, right = 0, len(arr) - 1
     result = -1
     while left <= right:
@@ -39,7 +39,7 @@ var kf=Object.defineProperty;var Tf=(n,e,t)=>e in n?kf(n,e,{enumerable:!0,config
             {{left = mid + 1}}
     return result
     `,extensions:[cs,Jn,ps(),v.editable.of(!1),v.lineWrapping],parent:document.getElementById("bsearch-first")});new v({doc:`def search_last(arr, target):
-    """search_any([1, 2, 2, 2, 3], 2) => 3"""
+    """search_last([1, 2, 2, 2, 3], 2) => 3"""
     left, right = 0, len(arr) - 1
     result = -1
     while left <= right:
